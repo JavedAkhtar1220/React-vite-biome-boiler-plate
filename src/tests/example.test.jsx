43959ea269f919f2de9bd6c1e2be834487f67894
shahnawazuvicuo <shahnawaz.hussain@uvicuo.com>
2024-08-27 01:19:37 +0500

    initial commit

---
+++ src/tests/example.test.jsx
@@ -0,0 +1,13 @@
+// import { expect, test } from 'bun:test';
+
+import { describe, expect, it } from 'vitest';
+
+describe('Test for checking', () => {
+  it('2 + 2 = 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  it('dummy test assertion', () => {
+    expect(true).toBe(true);
+  });
+});
