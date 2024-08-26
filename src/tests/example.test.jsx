76ac3150186825d711682a4100d15a93f8a4c934
shahnawazuvicuo <shahnawaz.hussain@uvicuo.com>
2024-08-27 03:15:02 +0500

    chore: rules added inside biome.json

--- src/tests/example.test.jsx
+++ src/tests/example.test.jsx
@@ -1,9 +1,9 @@
 // import { expect, test } from 'bun:test';
 
-import { describe, expect, it } from "vitest";
+import { describe, expect, it } from 'vitest';
 
-describe("Test for checking", () => {
-	it("2 + 2 = 4", () => {
+describe('Test for checking', () => {
+	it('2 + 2 = 4', () => {
 		expect(2 + 2).toBe(4);
 	});
 });
